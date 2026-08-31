57658a5696e03d121c5702bd3a044b21caf1c389
joacolevy <49836427@est.ort.edu.ar>
2026-08-31 09:07:24 -0300

    Initial commit

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform spawnPoint;
+    public bool isActive = false;
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isActive){
+            Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+        }
+
+        void OnCollisionEnter(Collision Col){
+            
+            isActive = true;
+        }
+    }
+}
---
+++ Assets/InstanciadorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanciadorScript : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+
+            GameObject clon;
+            clon = Instantiate (prefab, spawnPoint.position, Quaternion.identity);
+         Destroy(clon, 1);
+        }
+    }
+}
